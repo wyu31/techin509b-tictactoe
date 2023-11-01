137622194cc212a751ab0a6543491607310193e7
Wanling Yu <148395436+wyu31@users.noreply.github.com>
2023-10-31 18:02:56 -0700

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,8 +1,3 @@
-# This file is where game logic lives. No input
-# or output happens here. The logic in this file
-# should be unit-testable.
-
-
 def make_empty_board():
     return [
         [None, None, None],
@@ -14,9 +9,26 @@ def make_empty_board():
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
-    return None  # FIXME
+    # Check rows
+    for row in board:
+        if row[0] == row[1] == row[2] and row[0] is not None:
+            return row[0]
+
+    # Check columns
+    for column in range(3):
+        if board[0][column] == board[1][column] == board[2][column] and board[0][column] is not None:
+            return board[0][column]
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not None:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not None:
+        return board[0][2]
+
+    # No winner
+    return None
 
 
 def other_player(player):
     """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+    return 'O' if player == 'X' else 'X'
